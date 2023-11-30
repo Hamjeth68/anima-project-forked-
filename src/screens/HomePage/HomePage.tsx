5ca78c31eb83dd946150ef0399035f6a8fb6b9b7
CodeSandbox <support@codesandbox.io>
2023-11-30 20:18:51 +0000

    Initial commit

---
+++ src/screens/HomePage/HomePage.tsx
@@ -0,0 +1,246 @@
+import React from "react";
+
+export const HomePage = (): JSX.Element => {
+  return (
+    <div className="bg-white flex flex-row justify-center w-full">
+      <div className="bg-white w-[1920px] h-[7208px] relative">
+        <div className="inline-flex items-start gap-[907px] absolute top-[7139px] left-[106px]">
+          <div className="inline-flex items-start gap-[84px] relative flex-[0_0_auto]">
+            <div className="relative w-fit mt-[-1.00px] [font-family:'Lato',Helvetica] font-normal text-black text-[18px] tracking-[0] leading-[normal]">
+              Terms and conditions
+            </div>
+            <div className="relative w-fit mt-[-1.00px] [font-family:'Lato',Helvetica] font-normal text-black text-[18px] tracking-[0] leading-[normal]">
+              Privacy policy
+            </div>
+          </div>
+          <div className="inline-flex items-start gap-[84px] relative flex-[0_0_auto]">
+            <p className="relative w-fit mt-[-1.00px] [font-family:'Lato',Helvetica] font-normal text-black text-[18px] text-right tracking-[0] leading-[normal]">
+              Â© 2021 company name. All rights reserved worldwide.
+            </p>
+          </div>
+        </div>
+        <footer className="absolute w-[1708px] h-[625px] top-[6484px] left-[106px] bg-[#1c1c1c] rounded-[60px] overflow-hidden">
+          <p className="absolute w-[654px] top-[68px] left-[64px] [font-family:'Lato',Helvetica] font-bold text-white text-[66px] tracking-[0] leading-[101px]">
+            Vision Intelligence for Pet Toxicology
+          </p>
+          <div className="inline-flex flex-col items-start gap-[35px] absolute top-[377px] left-[64px]">
+            <div className="relative w-fit mt-[-1.00px] [font-family:'Lato',Helvetica] font-normal text-white text-[18px] tracking-[0] leading-[normal]">
+              Home
+            </div>
+            <div className="relative w-fit [font-family:'Lato',Helvetica] font-normal text-white text-[18px] tracking-[0] leading-[normal]">
+              Community
+            </div>
+            <div className="relative w-fit [font-family:'Lato',Helvetica] font-normal text-white text-[18px] tracking-[0] leading-[normal]">
+              Poisons
+            </div>
+            <div className="relative w-fit [font-family:'Lato',Helvetica] font-normal text-white text-[18px] tracking-[0] leading-[normal]">
+              Try Toxipets APP
+            </div>
+          </div>
+          <p className="absolute top-[376px] left-[1138px] [font-family:'Lato',Helvetica] font-normal text-white text-[18px] tracking-[0] leading-[normal]">
+            Find out more at Toxipets
+          </p>
+          <img
+            className="absolute w-[323px] h-[61px] top-[425px] left-[1138px] object-cover"
+            alt="Group"
+            src="https://c.animaapp.com/ltjoxlu9/img/group-4389-1@2x.png"
+          />
+          <img
+            className="absolute w-[1600px] h-px top-[312px] left-[43px] object-cover"
+            alt="Line"
+            src="https://c.animaapp.com/ltjoxlu9/img/line-1.svg"
+          />
+        </footer>
+        <div className="inline-flex items-center gap-[335px] absolute top-[4245px] left-[316px]">
+          <img
+            className="w-[365px] h-[753px] relative object-cover"
+            alt="Group"
+            src="https://c.animaapp.com/ltjoxlu9/img/group-4393.png"
+          />
+          <div className="inline-flex flex-col items-start gap-[20px] relative flex-[0_0_auto]">
+            <p className="relative w-[419px] mt-[-1.00px] [font-family:'Lato',Helvetica] font-bold text-black text-[64px] tracking-[0] leading-[70px]">
+              Join a Vibrant Community of Pet Owners
+            </p>
+            <p className="relative w-[517px] [font-family:'Lato',Helvetica] font-normal text-black text-[24px] tracking-[0.72px] leading-[34px]">
+              Explore our community hub where vets share videos, articles on toxicity, and answer all your pet safety
+              questions
+            </p>
+          </div>
+        </div>
+        <div className="flex w-[1335px] items-center justify-between absolute top-[5425px] left-[316px]">
+          <div className="inline-flex flex-col items-start gap-[20px] relative flex-[0_0_auto]">
+            <div className="relative w-[419px] mt-[-1.00px] [font-family:'Lato',Helvetica] font-bold text-black text-[64px] tracking-[0] leading-[70px]">
+              Personalized Toxicity Calculator
+            </div>
+            <p className="relative w-[517px] [font-family:'Lato',Helvetica] font-normal text-black text-[24px] tracking-[0.72px] leading-[34px]">
+              Get tailored advice on potential toxins for your pet’s weight and the recommended steps to keep them safe
+              (Coming soon)
+            </p>
+          </div>
+          <img
+            className="w-[691px] h-[594px] relative object-cover"
+            alt="Group"
+            src="https://c.animaapp.com/ltjoxlu9/img/group-4490.png"
+          />
+        </div>
+        <div className="inline-flex items-center gap-[335px] absolute top-[3499px] left-[316px]">
+          <div className="inline-flex flex-col items-start gap-[20px] relative flex-[0_0_auto]">
+            <div className="relative w-[327px] mt-[-1.00px] [font-family:'Lato',Helvetica] font-bold text-black text-[64px] tracking-[0] leading-[70px]">
+              FDA Food Recall
+            </div>
+            <p className="relative w-[517px] [font-family:'Lato',Helvetica] font-normal text-black text-[24px] tracking-[0.72px] leading-[34px]">
+              Stay informed about pet food safety. Get instant alerts on any FDA recalls for the brand your pet loves
+              (Coming soon)
+            </p>
+          </div>
+          <img
+            className="w-[395px] h-[319px] relative object-cover"
+            alt="Group"
+            src="https://c.animaapp.com/ltjoxlu9/img/group-4496@2x.png"
+          />
+        </div>
+        <div className="absolute w-[1920px] h-[923px] top-[2188px] left-0 bg-[#041a1a]">
+          <div className="flex w-[1287px] items-center gap-[335px] relative top-[92px] left-[291px]">
+            <img
+              className="w-[410px] h-[740px] relative object-cover"
+              alt="Group"
+              src="https://c.animaapp.com/ltjoxlu9/img/group-4495.png"
+            />
+            <div className="inline-flex flex-col items-start gap-[30px] relative flex-[0_0_auto]">
+              <div className="inline-flex flex-col items-start gap-[20px] relative flex-[0_0_auto]">
+                <p className="relative w-[491px] mt-[-1.00px] [font-family:'Lato',Helvetica] font-bold text-white text-[64px] tracking-[0] leading-[70px]">
+                  Consult our AI veterinary expert
+                </p>
+                <p className="relative w-[517px] [font-family:'Lato',Helvetica] font-normal text-white text-[24px] tracking-[0.72px] leading-[34px]">
+                  Ask our chatbot about common food, recipes and product toxicity
+                </p>
+              </div>
+              <img
+                className="relative w-[323px] h-[61px] object-cover"
+                alt="Group"
+                src="https://c.animaapp.com/ltjoxlu9/img/group-4389-1-2@2x.png"
+              />
+            </div>
+          </div>
+        </div>
+        <div className="inline-flex items-center gap-[335px] absolute top-[1235px] left-[316px]">
+          <div className="inline-flex flex-col items-start gap-[20px] relative flex-[0_0_auto]">
+            <div className="relative w-[224px] mt-[-1.00px] [font-family:'Lato',Helvetica] font-bold text-black text-[64px] tracking-[0] leading-[70px]">
+              Camera Search
+            </div>
+            <p className="relative w-[517px] [font-family:'Lato',Helvetica] font-normal text-black text-[24px] tracking-[0.72px] leading-[34px]">
+              Get vet-verified toxicity info instantly by scanning barcodes, reading package labels, and identifying
+              plants.
+            </p>
+          </div>
+          <img
+            className="w-[364px] h-[740px] relative object-cover"
+            alt="Group"
+            src="https://c.animaapp.com/ltjoxlu9/img/group-4391.png"
+          />
+        </div>
+        <div className="absolute w-[1920px] h-[976px] top-[107px] left-0">
+          <div className="relative h-[996px] top-[-20px]">
+            <div className="absolute w-[1920px] h-[996px] top-0 left-0">
+              <img
+                className="absolute w-[1920px] h-[976px] top-[20px] left-0"
+                alt="Mask"
+                src="https://c.animaapp.com/ltjoxlu9/img/mask.png"
+              />
+            </div>
+            <div className="inline-flex items-center gap-[114px] absolute top-[149px] left-[149px]">
+              <div className="inline-flex flex-col items-start relative flex-[0_0_auto]">
+                <img
+                  className="relative w-[150px] h-[150px] object-cover"
+                  alt="Element"
+                  src="https://c.animaapp.com/ltjoxlu9/img/512-2x-1@2x.png"
+                />
+                <div className="inline-flex flex-col items-start gap-[38px] relative flex-[0_0_auto]">
+                  <div className="inline-flex flex-col items-start gap-[24px] relative flex-[0_0_auto]">
+                    <div className="inline-flex flex-col items-start relative flex-[0_0_auto]">
+                      <div className="relative w-fit mt-[-1.00px] [font-family:'Lato',Helvetica] font-black text-black text-[115px] tracking-[3.45px] leading-[normal] whitespace-nowrap">
+                        ToxiPets
+                      </div>
+                      <div className="relative w-fit [font-family:'Lato',Helvetica] font-bold text-[#008080] text-[34px] tracking-[1.02px] leading-[normal]">
+                        Pet Safety Simplified
+                      </div>
+                    </div>
+                    <p className="relative w-fit [font-family:'Lato',Helvetica] font-normal text-black text-[24px] tracking-[0.72px] leading-[normal]">
+                      Find out what your pet can or cannot eat
+                    </p>
+                  </div>
+                  <div className="inline-flex flex-col items-start gap-[8px] px-[20px] py-[19px] relative flex-[0_0_auto] bg-white rounded-[20px] overflow-hidden border border-solid border-[#cbcbc4]">
+                    <div className="inline-flex flex-col items-start gap-[15px] relative flex-[0_0_auto]">
+                      <div className="inline-flex items-center gap-[14px] relative flex-[0_0_auto]">
+                        <img
+                          className="relative w-[99px] h-[99px] object-cover"
+                          alt="Group"
+                          src="https://c.animaapp.com/ltjoxlu9/img/group-4388-2x-1@2x.png"
+                        />
+                        <div className="relative w-[202px] h-[78px] [font-family:'Poppins',Helvetica] font-semibold text-black text-[30px] tracking-[1.71px] leading-[35px]">
+                          Try Toxipets App
+                        </div>
+                      </div>
+                      <img
+                        className="relative w-[323px] h-[61px] object-cover"
+                        alt="Group"
+                        src="https://c.animaapp.com/ltjoxlu9/img/group-4389-1-2@2x.png"
+                      />
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className="relative w-[1049px] h-[740px]">
+                <div className="relative h-[740px]">
+                  <img
+                    className="absolute w-[740px] h-[740px] top-0 left-[309px] object-cover"
+                    alt="Cute pet collage"
+                    src="https://c.animaapp.com/ltjoxlu9/img/cute-pet-collage-isolated-1.png"
+                  />
+                  <img
+                    className="absolute w-[366px] h-[740px] top-0 left-0 object-cover"
+                    alt="Group"
+                    src="https://c.animaapp.com/ltjoxlu9/img/group-4390-2x-1.png"
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <header className="absolute w-[1920px] h-[107px] top-0 left-0 bg-white shadow-[0px_6px_3px_#c4d3f01a]">
+          <img
+            className="absolute w-[139px] h-[37px] top-[35px] left-[106px] object-cover"
+            alt="Logo"
+            src="https://c.animaapp.com/ltjoxlu9/img/logo-2x-1@2x.png"
+          />
+          <div className="inline-flex items-start gap-[68px] absolute top-[42px] left-[692px]">
+            <div className="relative w-fit mt-[-1.00px] [font-family:'Lato',Helvetica] font-normal text-[#333333] text-[18px] tracking-[0] leading-[normal]">
+              Home
+            </div>
+            <div className="relative w-fit mt-[-1.00px] [font-family:'Lato',Helvetica] font-normal text-[#333333] text-[18px] tracking-[0] leading-[normal]">
+              Community
+            </div>
+            <div className="relative w-fit mt-[-1.00px] [font-family:'Lato',Helvetica] font-normal text-[#333333] text-[18px] tracking-[0] leading-[normal]">
+              Poisons
+            </div>
+            <div className="relative w-fit mt-[-1.00px] [font-family:'Lato',Helvetica] font-normal text-[#333333] text-[18px] tracking-[0] leading-[normal]">
+              Try Toxipets APP
+            </div>
+          </div>
+          <div className="inline-flex items-start gap-[24px] absolute top-[26px] left-[1532px]">
+            <div className="inline-flex h-[55px] items-center justify-center gap-[8px] px-[33px] py-0 relative flex-[0_0_auto] bg-[#e0e0e0] rounded-[54px] overflow-hidden shadow-[0px_0px_16px_#0000000d]">
+              <div className="relative w-fit [font-family:'Lato',Helvetica] font-bold text-[#484848] text-[18px] tracking-[0] leading-[normal]">
+                Get App
+              </div>
+            </div>
+            <div className="inline-flex h-[55px] items-center justify-center gap-[8px] px-[41px] py-0 relative flex-[0_0_auto] bg-[#008080] rounded-[54px] overflow-hidden border border-solid shadow-[0px_0px_16px_#00000026]">
+              <div className="relative w-fit [font-family:'Lato',Helvetica] font-bold text-white text-[18px] tracking-[0] leading-[normal]">
+                Login
+              </div>
+            </div>
+          </div>
+        </header>
+      </div>
+    </div>
+  );
+};
